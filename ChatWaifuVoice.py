5d176eff949c1db98bb5d2d754ba0210c2af156f
Stardust·减 <52093397+Stardust-minus@users.noreply.github.com>
2022-12-25 20:43:18 +0800

    Update ChatWaifuVoice.py

--- ChatWaifuVoice.py
+++ ChatWaifuVoice.py
@@ -1,23 +1,13 @@
-from scipy.io.wavfile import write
-from mel_processing import spectrogram_torch
-from text import text_to_sequence, _clean_text
-from models import SynthesizerTrn
 import utils
 import commons
 import sys
 import re
-from torch import no_grad, LongTensor
 import logging
-from winsound import PlaySound
+import winsound 
 import argparse
 import queue
 import sounddevice as sd
 from vosk import Model, KaldiRecognizer
-
-chinese_model_path = ".\model\CN\model.pth"
-chinese_config_path = ".\model\CN\config.json"
-japanese_model_path = ".\model\H_excluded.pth"
-japanese_config_path = ".\model\config.json"
 
 q = queue.Queue()
 def int_or_str(text):
@@ -84,33 +74,37 @@ except KeyboardInterrupt:
 from pyChatGPT import ChatGPT
 import json
 
-modelmessage = """ID      Output Language
-0       Chinese
-1       Japanese
-"""
 
-idmessage_cn = """ID      Speaker
-0       綾地寧々
-1       在原七海
-2       小茸
-3       唐乐吟
+idmessage_cn = """      
+Speaker
+['派蒙','空','荧','阿贝多','枫原万叶','温迪','八重神子','纳西妲','钟离','诺艾尔','凝光','托马',
+           '北斗','莫娜','荒泷一斗','提纳里','芭芭拉','艾尔海森','雷电将军','赛诺','琴','班尼特','五郎',
+           '神里绫华','迪希雅','夜兰','辛焱','安柏','宵宫','云堇','妮露','烟绯','鹿野院平藏','凯亚','达达利亚',
+           '迪卢克','可莉','早柚','香菱','重云','刻晴','久岐忍','珊瑚宫心海','迪奥娜','戴因斯雷布','魈',
+           '神里绫人','丽莎','优菈','凯瑟琳','雷泽','菲谢尔','九条裟罗','甘雨','行秋','胡桃','迪娜泽黛',
+           '柯莱','申鹤','砂糖','萍姥姥','奥兹','罗莎莉亚','式大将','哲平','坎蒂丝','托克','留云借风真君',
+           '昆钧','塞琉斯','多莉','大肉丸','莱依拉','散兵','拉赫曼','杜拉夫','阿守','玛乔丽','纳比尔',
+           '海芭夏','九条镰治','阿娜耶','阿晃','阿扎尔','七七','博士','白术','埃洛伊','大慈树王','女士',
+           '丽塔','失落迷迭','缭乱星棘','伊甸','伏特加女孩','狂热蓝调','莉莉娅','萝莎莉娅','八重樱','八重霞',
+           '卡莲','第六夜想曲','卡萝尔','姬子','极地战刃','布洛妮娅','次生银翼','理之律者','迷城骇兔','希儿',
+           '魇夜星渊','黑希儿','帕朵菲莉丝','天元骑英','幽兰黛尔','德丽莎','月下初拥','朔夜观星','暮光骑士',
+           '明日香','李素裳','格蕾修','梅比乌斯','渡鸦','人之律者','爱莉希雅','爱衣','天穹游侠','琪亚娜',
+           '空之律者','薪炎之律者','云墨丹心','符华','识之律者','维尔薇','芽衣','雷之律者','阿波尼亚']
 """
-
-idmessage_jp = """ID      Speaker
-0       綾地寧々
-1       因幡めぐる
-2       朝武芳乃
-3       常陸茉子
-4       ムラサメ
-5       鞍馬小春
-6       在原七海
-"""
-
 inputLanguage = """ID      Input Language
 0       Chinese
 1       Japanese
 2       English
 """
+
+def get_language_id(message):
+    speaker_id = input(message)
+    try:
+        speaker_id = int(speaker_id)
+    except:
+        print(str(speaker_id) + ' is not a valid ID!')
+        sys.exit(1)
+    return speaker_id
 
 def voice_input_jp():
     model = Model(lang="cn")
@@ -152,7 +146,7 @@ def voice_input_cn():
             if dump_fn is not None:
                 dump_fn.write(data)
 
-def voice_input_jpjp():
+def voice_input_jpzh():
     model = Model(lang="ja")
     print("You:")
     with sd.RawInputStream(samplerate=args.samplerate, blocksize=8000, device=args.device,
@@ -167,12 +161,12 @@ def voice_input_jpjp():
                 a = ''.join(a.split())
                 if(len(a) > 0):
                     print(a)
-                    user_input = a + " 使用日本语"
+                    user_input = a + " 使用中文"
                     return user_input
             if dump_fn is not None:
                 dump_fn.write(data)
 
-def voice_input_enjp():
+def voice_input_enzh():
     model = Model(lang="en-us")
     print("You:")
     with sd.RawInputStream(samplerate=args.samplerate, blocksize=8000, device=args.device,
@@ -187,7 +181,7 @@ def voice_input_enjp():
                 a = ''.join(a.split())
                 if(len(a) > 0):
                     print(a)
-                    user_input = a + " 使用日本语"
+                    user_input = a + " 使用中文"
                     return user_input
             if dump_fn is not None:
                 dump_fn.write(data)
@@ -199,187 +193,41 @@ def get_token():
 
       
 ################################################
-logging.getLogger('numba').setLevel(logging.WARNING)
-
-def get_text(text, hps, cleaned=False):
-    if cleaned:
-        text_norm = text_to_sequence(text, hps.symbols, [])
-    else:
-        text_norm = text_to_sequence(text, hps.symbols, hps.data.text_cleaners)
-    if hps.data.add_blank:
-        text_norm = commons.intersperse(text_norm, 0)
-    text_norm = LongTensor(text_norm)
-    return text_norm
-
-def get_speaker_id(message):
-    speaker_id = input(message)
-    try:
-        speaker_id = int(speaker_id)
-    except:
-        print(str(speaker_id) + ' is not a valid ID!')
-        sys.exit(1)
-    return speaker_id
-
-def get_model_id(message):
-    speaker_id = input(message)
-    try:
-        speaker_id = int(speaker_id)
-    except:
-        print(str(speaker_id) + ' is not a valid ID!')
-        sys.exit(1)
-    return speaker_id
-
-def get_language_id(message):
-    speaker_id = input(message)
-    try:
-        speaker_id = int(speaker_id)
-    except:
-        print(str(speaker_id) + ' is not a valid ID!')
-        sys.exit(1)
-    return speaker_id
-
-
-def get_label_value(text, label, default, warning_name='value'):
-    value = re.search(rf'\[{label}=(.+?)\]', text)
-    if value:
-        try:
-            text = re.sub(rf'\[{label}=(.+?)\]', '', text, 1)
-            value = float(value.group(1))
-        except:
-            print(f'Invalid {warning_name}!')
-            sys.exit(1)
-    else:
-        value = default
-    return value, text
-
-
-def get_label(text, label):
-    if f'[{label}]' in text:
-        return True, text.replace(f'[{label}]', '')
-    else:
-        return False, text
-
-
-def generateSound(inputString, id, model_id):
+def generateSound(text, speaker):
     if '--escape' in sys.argv:
         escape = True
     else:
         escape = False
-
-    #model = input('0: Chinese')
-    #config = input('Path of a config file: ')
-    if model_id == 0:
-        model = chinese_model_path
-        config = chinese_config_path
-    elif model_id == 1:
-        model = japanese_model_path
-        config = japanese_config_path
-        
-
-    hps_ms = utils.get_hparams_from_file(config)
-    n_speakers = hps_ms.data.n_speakers if 'n_speakers' in hps_ms.data.keys() else 0
-    n_symbols = len(hps_ms.symbols) if 'symbols' in hps_ms.keys() else 0
-    emotion_embedding = hps_ms.data.emotion_embedding if 'emotion_embedding' in hps_ms.data.keys() else False
-
-    net_g_ms = SynthesizerTrn(
-        n_symbols,
-        hps_ms.data.filter_length // 2 + 1,
-        hps_ms.train.segment_size // hps_ms.data.hop_length,
-        n_speakers=n_speakers,
-        emotion_embedding=emotion_embedding,
-        **hps_ms.model)
-    _ = net_g_ms.eval()
-    utils.load_checkpoint(model, net_g_ms)
-
-    if n_symbols != 0:
-        if not emotion_embedding:
-            #while True:
-            if(1 == 1):
-                choice = 't'
-                if choice == 't':
-                    text = inputString
-                    if text == '[ADVANCED]':
-                        text = "我不会说"
-
-                    length_scale, text = get_label_value(
-                        text, 'LENGTH', 1, 'length scale')
-                    noise_scale, text = get_label_value(
-                        text, 'NOISE', 0.667, 'noise scale')
-                    noise_scale_w, text = get_label_value(
-                        text, 'NOISEW', 0.8, 'deviation of noise')
-                    cleaned, text = get_label(text, 'CLEANED')
-
-                    stn_tst = get_text(text, hps_ms, cleaned=cleaned)
-                    
-                    speaker_id = id 
-                    out_path = "output.wav"
-
-                    with no_grad():
-                        x_tst = stn_tst.unsqueeze(0)
-                        x_tst_lengths = LongTensor([stn_tst.size(0)])
-                        sid = LongTensor([speaker_id])
-                        audio = net_g_ms.infer(x_tst, x_tst_lengths, sid=sid, noise_scale=noise_scale,
-                                               noise_scale_w=noise_scale_w, length_scale=length_scale)[0][0, 0].data.cpu().float().numpy()
-
-                write(out_path, hps_ms.data.sampling_rate, audio)
-                print('Successfully saved!')
+        url_sound = voice_api + "?speaker=" + speaker + "&text=" + text + "&format=wav"
+        sound_bytes = requests.get(url=url_sound).content
+        return sound_bytes
 
 if __name__ == "__main__":
     session_token = get_token()
     api = ChatGPT(session_token)
-
     print(inputLanguage)
     language_id = get_language_id("选择输入语言：")
-    if language_id == 0: #cn
-        print(modelmessage)
-        model_id = get_model_id('选择回复语言: ')
-        if model_id == 0:
-            print("\n" + idmessage_cn)
-            id = get_speaker_id('选择角色: ')
-        elif model_id == 1:
-            print("\n" + idmessage_jp)
-            id = get_speaker_id('选择角色: ')
-    elif language_id == 1: #jp
-        model_id = 1
-        print("\n" + idmessage_jp)
-        id = get_speaker_id('选择角色: ')
-    elif language_id == 2: #en
-        model_id = 1
-        print("\n" + idmessage_cn)
-        id = get_speaker_id('选择角色: ')
-
+    print("\n" + idmessage_cn)
+    speaker = input('选择角色: ')
     print()
     while True:
-
-        if language_id == 0 and model_id == 0: #input=cn output=cn
+        if language_id == 0 : #input=cn
             resp = api.send_message(voice_input_cn())
             if(resp == "quit()"):
                 break
             answer = resp["message"].replace('\n','')
             print("ChatGPT:")
             print(answer)
-            generateSound("[ZH]"+answer+"[ZH]", id, model_id)
-            PlaySound(r'.\output.wav', flags=1)
-        elif language_id == 0 and model_id == 1: #input=cn output=jp
-            resp = api.send_message(voice_input_jp())
-            if(resp == "quit()"):
-                break
+            winsound.PlaySound(generateSound(answer, speaker), winsound.SND_MEMORY)
+        elif language_id == 1: #input=jp
+            resp = api.send_message(voice_input_jpzh())
             answer = resp["message"].replace('\n','')
             print("ChatGPT:")
             print(answer)
-            generateSound(answer, id, model_id)
-            PlaySound(r'.\output.wav', flags=1)
-        elif language_id == 1: #input=jp output=jp
-            resp = api.send_message(voice_input_jpjp())
+            winsound.PlaySound(generateSound(answer, speaker), winsound.SND_MEMORY)
+        elif language_id == 2: #input=en
+            resp = api.send_message(voice_input_enzh())
             answer = resp["message"].replace('\n','')
             print("ChatGPT:")
             print(answer)
-            generateSound(answer, id, model_id)
-            PlaySound(r'.\output.wav', flags=0)
-        elif language_id == 2: #input=en output=jp
-            resp = api.send_message(voice_input_enjp())
-            answer = resp["message"].replace('\n','')
-            print("ChatGPT:")
-            print(answer)
-            generateSound(answer, id, model_id)
-            PlaySound(r'.\output.wav', flags=0)
+            winsound.PlaySound(generateSound(answer, speaker), winsound.SND_MEMORY)
